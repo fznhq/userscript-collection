e926be61b68add43d21868c29d8a3e6b3e96b87c
Fauzan Haq <55099255+fznhq@users.noreply.github.com>
2023-12-25 18:18:34 +0700

    Update Youtube_Fullpage_Theater.user.js
    * version

--- Youtube_Fullpage_Theater.user.js
+++ Youtube_Fullpage_Theater.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Youtube Fullpage Theater
-// @version      0.7.3
+// @version      0.7.4
 // @description  Make theater mode fill the entire page view with hidden navbar
 // @run-at       document-body
 // @match        https://www.youtube.com/*
