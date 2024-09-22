e0200e5a98dfc202ccac92f5caff7426d8ab1bfb
Fauzan Haq <55099255+fznhq@users.noreply.github.com>
2024-09-22 18:58:13 +0700

    Update Youtube_Quality_HD.user.js
    * Add support youtube short

--- Youtube_Quality_HD.user.js
+++ Youtube_Quality_HD.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Youtube Quality HD
-// @version      1.0.1
+// @version      1.1.0
 // @description  Automatically select your desired video quality and select premium when posibble.
 // @run-at       document-body
 // @match        https://www.youtube.com/*
@@ -38,6 +38,9 @@
         premium: `{"svg":{"viewBox":"0 0 123 95"},"path":{"fill":"white","d":"M1 28 20 1a3 3 0 0 1 3-1h77a3 3 0 0 1 3 1l19 27a3 3 0 0 1 1 2 3 3 0 0 1-1 2L64 94a3 3 0 0 1-4 0L1 32a3 3 0 0 1-1-1 3 3 0 0 1 1-3Zm44 5 17 51 17-51Zm39 0L68 82l46-49ZM56 82 39 33H9l47 49ZM28 5l13 20L56 5Zm39 0 15 20L95 5Zm33 2L87 27h28L100 7ZM77 27 61 7 47 27Zm-41 0L22 7 8 27Z"}}`,
         quality: `{"svg":{"viewBox":"0 0 123 107"},"path":{"fill":"white","fill-rule":"evenodd","d":"M113 57a4 4 0 0 1 2 1l3 4a5 5 0 0 1 1 2 4 4 0 0 1 0 1 4 4 0 0 1 0 2 4 4 0 0 1-1 1l-3 2v1l1 1v2h3a4 4 0 0 1 3 1 4 4 0 0 1 1 2 4 4 0 0 1 0 1l-1 6a4 4 0 0 1-1 3 4 4 0 0 1-3 1h-3l-1 1-1 1v1l2 2a4 4 0 0 1 1 1 4 4 0 0 1-1 3 4 4 0 0 1-1 2l-4 3a4 4 0 0 1-1 1 4 4 0 0 1-2 0 5 5 0 0 1-1 0 4 4 0 0 1-2-1l-2-3a1 1 0 0 1 0 1h-3v3a4 4 0 0 1-1 2 4 4 0 0 1-1 1 4 4 0 0 1-1 1 4 4 0 0 1-2 0h-5a4 4 0 0 1-4-5v-3l-2-1-1-1-2 2a4 4 0 0 1-2 1 4 4 0 0 1-1 0 4 4 0 0 1-2 0 4 4 0 0 1-1-1l-4-4a5 5 0 0 1 0-2 4 4 0 0 1-1-2 4 4 0 0 1 2-3l2-2v-1l-1-2h-2a4 4 0 0 1-2-1 4 4 0 0 1-1-1 4 4 0 0 1-1-1 4 4 0 0 1 0-2v-5a4 4 0 0 1 1-2 5 5 0 0 1 1-1 4 4 0 0 1 1-1 4 4 0 0 1 2 0h3l1-1v-2l-1-2a4 4 0 0 1-1-1 4 4 0 0 1 0-2 4 4 0 0 1 0-2 4 4 0 0 1 1-1l4-3a5 5 0 0 1 2-1 4 4 0 0 1 1-1 4 4 0 0 1 2 1 4 4 0 0 1 1 1l2 2h2l1-1 1-2a4 4 0 0 1 0-2 4 4 0 0 1 1-1 4 4 0 0 1 2-1 4 4 0 0 1 1 0h6a5 5 0 0 1 1 1 4 4 0 0 1 2 1 4 4 0 0 1 0 1 4 4 0 0 1 1 2l-1 3h1l1 1 1 1 3-2a4 4 0 0 1 1-1 4 4 0 0 1 2 0 4 4 0 0 1 1 0ZM11 0h82a11 11 0 0 1 11 11v30h-1a11 11 0 0 0-2-1h-2V21H5v49h51a12 12 0 0 0 0 2v4h-1v11h4l1 1h1l-1 1a12 12 0 0 0 0 2v1H11A11 11 0 0 1 0 81V11A11 11 0 0 1 11 0Zm35 31 19 13a3 3 0 0 1 0 4L47 61a3 3 0 0 1-2 0 3 3 0 0 1-3-2V33l1-1a3 3 0 0 1 3-1Zm4 56V76H29v11ZM24 76H5v5a6 6 0 0 0 6 6h13V76Zm52-60V5H55v11Zm5-11v11h18v-5a6 6 0 0 0-6-6ZM50 16V5H29v11Zm-26 0V5H11a6 6 0 0 0-6 6v5Zm70 56a6 6 0 1 1-6 7 6 6 0 0 1 6-7Zm-1-8a14 14 0 1 1-13 16 14 14 0 0 1 13-16Z"}}`,
     };
+
+    const allowedIds = ["movie_player", "shorts-player"];
+    const cachePlayers = new Map();
 
     /**
      * @param {string} name
@@ -87,7 +90,6 @@
     }
 
     const element = {
-        player: $("#movie_player"),
         settings: $(".ytp-settings-menu"),
         panel_settings: $(".ytp-settings-menu .ytp-panel-menu"),
         quality_menu: $(".ytp-quality-menu", false),
@@ -134,9 +136,12 @@
      * @property {boolean} isPlayable
      */
 
-    function setCurrentMaxQuality() {
+    /**
+     * @param {HTMLElement} player
+     */
+    function setCurrentMaxQuality(player) {
         /** @type {QualityData[]} */
-        const qualityData = element.player().getAvailableQualityData();
+        const qualityData = player.getAvailableQualityData();
         if (qualityData.length) {
             currentMaxQuality = Math.max(
                 ...qualityData.map((data) => parseLabel(data.qualityLabel))
@@ -164,6 +169,18 @@
     }
 
     /**
+     * @param {HTMLVideoElement} video
+     * @returns {HTMLElement}
+     */
+    function findPlayer(video) {
+        if (cachePlayers.has(video)) return cachePlayers.get(video);
+        let elem = video.parentElement;
+        while (elem && !allowedIds.includes(elem.id)) elem = elem.parentElement;
+        if (elem) cachePlayers.set(video, elem);
+        return elem;
+    }
+
+    /**
      * @typedef {object} Player
      * @property {() => string} getPlaybackQualityLabel
      * @property {() => QualityData[]} getAvailableQualityData
@@ -172,13 +189,17 @@
 
     let isUpdated = false;
 
+    /**
+     * @param {Player} player
+     */
     function setVideoQuality() {
         if (manualOverride) return;
         if (isUpdated) return (isUpdated = false);
-        if (!currentMaxQuality) setCurrentMaxQuality();
 
         /** @type {Player} */
-        const player = element.player();
+        const player = findPlayer(this);
+        if (!currentMaxQuality) setCurrentMaxQuality(player);
+        const short = player.id.includes("short");
         const label = player.getPlaybackQualityLabel();
         const quality = parseLabel(label);
         const prefer = getPreferredQuality();
@@ -187,7 +208,8 @@
         if (
             quality &&
             listQuality.includes(quality) &&
-            (isUpdated = quality != prefer || selected.qualityLabel != label)
+            (isUpdated =
+                quality != prefer || (!short && selected.qualityLabel != label))
         ) {
             player.setPlaybackQualityRange(
                 selected.quality,
@@ -244,7 +266,7 @@
         menu.item.addEventListener("click", function () {
             setChecked(this, saveOption(optName, !options[optName]));
             triggerSyncOptions();
-            setVideoQuality();
+            setVideoQuality.call(element.video());
         });
 
         return (element.premium_menu = menu.item);
@@ -286,7 +308,7 @@
                 const newValue = saveOption(optName, listQuality[pos]);
                 setTextQuality(element.text_quality, newValue);
                 triggerSyncOptions();
-                setVideoQuality((manualOverride = false));
+                setVideoQuality.call(element.video(), (manualOverride = false));
             }
         });
 
@@ -302,20 +324,15 @@
         if (menu && listQuality.includes(quality)) manualOverride = true;
     }
 
-    function reset() {
-        isUpdated = false;
-        manualOverride = false;
-        setCurrentMaxQuality();
-    }
-
     async function syncOptions() {
         if ((await GM.getValue("updated_id")) != options.updated_id) {
             isUpdated = false;
             for (const name in options) options[name] = await GM.getValue(name);
             setChecked(element.premium_menu, options.preferred_premium);
             setTextQuality(element.text_quality, options.preferred_quality);
-            const video = element.video();
-            if (video && !video.paused) setVideoQuality();
+            for (const [video] of cachePlayers) {
+                if (!video.paused) setVideoQuality.call(video);
+            }
         }
     }
 
@@ -323,16 +340,37 @@
         setTimeout(() => syncOptions().then(checkOptions), 1e3);
     })();
 
+    /**
+     * @param {HTMLVideoElement} video
+     */
+    function addVideoListener(video) {
+        if (cachePlayers.has(video) || !findPlayer(video)) return;
+        const fn = setVideoQuality.bind(video);
+        video.addEventListener("play", () => setTimeout(fn, 200));
+        video.addEventListener("resize", fn);
+    }
+
+    /**
+     * @param {CustomEvent} ev
+     */
+    function playerUpdated(ev) {
+        const target = ev.target;
+        if (allowedIds.some((id) => target.querySelector("#" + id))) {
+            isUpdated = false;
+            manualOverride = false;
+            setCurrentMaxQuality(ev.detail);
+            addVideoListener(target.querySelector("video"));
+        }
+    }
+
     observer((_, observe) => {
-        const video = element.video();
+        if (!element.video()) return;
 
-        if (!video) return;
-
+        const videos = document.querySelectorAll("video");
+        videos.forEach((video) => addVideoListener(video));
         element.panel_settings().append(premiumMenu(), qualityMenu());
         element.settings().addEventListener("click", setOverride, true);
-        video.addEventListener("play", () => setTimeout(setVideoQuality, 200));
-        video.addEventListener("resize", setVideoQuality);
-        document.addEventListener("yt-player-updated", reset);
+        document.addEventListener("yt-player-updated", playerUpdated);
 
         observe.disconnect();
     }, document.body);
