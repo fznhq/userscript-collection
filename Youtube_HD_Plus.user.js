124aff85b393d3f6fa54f8fe471fe76b4c98cd07
Fauzan Haq <55099255+fznhq@users.noreply.github.com>
2025-05-06 23:13:26 +0700

    Update Youtube_HD_Plus.user.js
    * Version

--- Youtube_HD_Plus.user.js
+++ Youtube_HD_Plus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YouTube HD Plus
-// @version      2.1.9
+// @version      2.2.0
 // @description  Automatically select your desired video quality and select premium when posibble. (Support YouTube Desktop, Music & Mobile)
 // @run-at       document-body
 // @inject-into  content
